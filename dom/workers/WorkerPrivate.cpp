56c99d38b1e78fcc05b0d6719bb589ac38167674
Andrea Marchesini <amarchesini@mozilla.com>
2016-07-06 08:36:54 +0200

    Bug 1282992 - Improve sync event loop shutdown in workers, r=khuey a=ritu
    --HG--
    extra : source : 98590457779dabc3a19471c400985d14072f931a
    extra : intermediate-source : 1855158bd62c34b5c931e9092f196b305d392382

--- dom/workers/WorkerPrivate.cpp
+++ dom/workers/WorkerPrivate.cpp
@@ -5448,8 +5448,8 @@ WorkerPrivate::DestroySyncLoop(uint32_t aLoopIndex, nsIThreadInternal* aThread)
   MOZ_ALWAYS_SUCCEEDS(aThread->PopEventQueue(nestedEventTarget));
 
   if (!mSyncLoopStack.Length() && mPendingEventQueueClearing) {
-    ClearMainEventQueue(WorkerRan);
     mPendingEventQueueClearing = false;
+    ClearMainEventQueue(WorkerRan);
   }
 
   return result;
