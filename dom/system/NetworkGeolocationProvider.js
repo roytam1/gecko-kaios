a7c6753159fc516476019ab0ef7598e2e4d6745b
Jamin Liu <jamin.liu@kaiostech.com>
2016-12-22 12:11:22 +0800

    Bug 6334 - Remove getDeviceInfo() and get device_info via the utility module 'DeviceUtils'. r=Shawn

--- dom/system/NetworkGeolocationProvider.js
+++ dom/system/NetworkGeolocationProvider.js
@@ -496,56 +496,10 @@ WifiGeoPositionProvider.prototype = {
     this.sendLocationRequest(null);
   },
 
-  getDeviceInfo: function() {
-    let deferred = Promise.defer();
-
-    let prefDeviceType;
-    let prefDeviceBrand;
-    let prefDeviceModel;
-    let prefDeviceOs;
-    let prefDeviceVersion;
-    try {
-      prefDeviceType    = Services.prefs.getIntPref(PREF_SERVICE_DEVICE_TYPE);
-      prefDeviceBrand   = Services.prefs.getCharPref(PREF_SERVICE_DEVICE_BRAND);
-      prefDeviceModel   = Services.prefs.getCharPref(PREF_SERVICE_DEVICE_MODEL);
-      prefDeviceOs      = Services.prefs.getCharPref(PREF_B2G_OS_NAME);
-      prefDeviceVersion = Services.prefs.getCharPref(PREF_B2G_VERSION);
-    } catch (e) {
-      LOG("Failed to get device info. " + e);
-    }
-
-    let device_info = {
-      device_type : prefDeviceType,
-      brand       : prefDeviceBrand,
-      model       : prefDeviceModel,
-      reference   : DeviceUtils.getRefNumber(),
-      os          : prefDeviceOs,
-      os_version  : prefDeviceVersion,
-      device_id   : ""
-    };
-
-    // TODO: handle dual-SIM case properly
-    let conn = gMobileConnectionService.getItemByServiceId(0);
-    conn.getDeviceIdentities({
-      notifyGetDeviceIdentitiesRequestSuccess: function(aResult) {
-        if (aResult.imei && parseInt(aResult.imei) !== 0) {
-          device_info.device_id = aResult.imei;
-        } else if (aResult.meid && parseInt(aResult.meid) !== 0) {
-          device_info.device_id = aResult.meid;
-        } else {
-          device_info.device_id = aResult.esn;
-        }
-        deferred.resolve(device_info);
-      }
-    });
-
-    return deferred.promise;
-  },
-
   getAccessToken: function () {
     let deferred = Promise.defer();
 
-    this.getDeviceInfo().then((device_info) => {
+    DeviceUtils.getTDeviceObject().then((device_info) => {
       let url = Services.urlFormatter.formatURLPref("geo.token.uri");
 
       let xhr = Components.classes["@mozilla.org/xmlextras/xmlhttprequest;1"]
