075f72e2252f837a4e53c3e13b7e3dff2521f98a
Steven Lee <Steven.Lee@kaiostech.com>
2017-01-17 17:27:08 +0800

    bug 7903: Fix the compiling errors on OSX, r=shelly

---
+++ hal/fallback/FallbackFlipManager.cpp
@@ -0,0 +1,30 @@
+#include "Hal.h"
+
+
+using namespace mozilla::hal;
+
+namespace mozilla {
+namespace hal_impl {
+
+void
+NotifyFlipStateFromInputDevice(bool)
+{
+}
+
+void
+RequestCurrentFlipState()
+{
+}
+
+void
+EnableFlipNotifications()
+{
+}
+
+void
+DisableFlipNotifications()
+{
+}
+
+} // hal_impl
+} // namespace mozilla
