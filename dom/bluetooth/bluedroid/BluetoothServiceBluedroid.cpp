1c4967dcd2c982065ee75d20fddc9ef9049268cc
Jamin Liu <jamin.liu@kaiostech.com>
2017-05-19 15:37:20 +0800

    Bug 13646 - Get properties of bonded device for updating Bluetooth CoD. r=jack.hung

--- dom/bluetooth/bluedroid/BluetoothServiceBluedroid.cpp
+++ dom/bluetooth/bluedroid/BluetoothServiceBluedroid.cpp
@@ -2652,6 +2652,11 @@ BluetoothServiceBluedroid::BondStateChangedNotification(
   } else {
     if (!mBondedAddresses.Contains(aRemoteBdAddr)) {
       mBondedAddresses.AppendElement(aRemoteBdAddr);
+
+      // Get properties of bonded device for updating CoD
+      sBtCoreInterface->GetRemoteDeviceProperties(aRemoteBdAddr,
+        new GetRemoteDevicePropertiesResultHandler(mGetDeviceRequests,
+        aRemoteBdAddr));
     }
 
     // We don't assert |!remotebdName.IsEmpty()| since empty string is also
