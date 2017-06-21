1c4f7dd18e6fdcbf9bb5a028c1be4c67493822b9
Solomon Chiu <solomon.chiu@kaiostech.com>
2017-06-21 17:15:15 +0800

    Bug 12246 - Increase buffer amount of framebuffer(from 2 to 3). r=seinlin

--- widget/gonk/libdisplay/FramebufferSurface.cpp
+++ widget/gonk/libdisplay/FramebufferSurface.cpp
@@ -41,7 +41,7 @@
 #include "GraphicBufferAlloc.h"
 
 #ifndef NUM_FRAMEBUFFER_SURFACE_BUFFERS
-#define NUM_FRAMEBUFFER_SURFACE_BUFFERS (2)
+#define NUM_FRAMEBUFFER_SURFACE_BUFFERS (3)
 #endif
 
 // ----------------------------------------------------------------------------
