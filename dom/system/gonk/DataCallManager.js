8d580debfe839d670da2ba9913ebe92a2173b137
Cameron.Cheng <cameron.cheng@kaiostech.com>
2017-08-29 15:36:41 +0800

    Bug 21478 - [Data] Reset the retry counter when disconnect this data connection. r=Shawn

--- dom/system/gonk/DataCallManager.js
+++ dom/system/gonk/DataCallManager.js
@@ -1429,6 +1429,8 @@ DataCall.prototype = {
   reset: function() {
     this.resetLinkInfo();
 
+    //Reset the retry counter.
+    this.apnRetryCounter = 0;
     this.state = NETWORK_STATE_UNKNOWN;
   },
 
