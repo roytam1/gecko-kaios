1f5107fe33801bee99fb7bc8aa946c14f842a816
Fei Gao <fei.gao@kaiostech.com>
2017-12-07 18:09:32 -0800

    Bug 28628 change fast path on CSP load, r=fabrice
    (cherry picked from commit 9b32693445c748676d5f4a7e8bffe7d03b71e316)

--- dom/security/nsCSPService.cpp
+++ dom/security/nsCSPService.cpp
@@ -150,11 +150,12 @@ CSPService::ShouldLoad(uint32_t aContentType,
 
   if (status == nsIPrincipal::APP_STATUS_CERTIFIED) {
    // The CSP for certified apps is :
-   // "default-src * data: blob:; script-src 'self'; object-src 'none'; style-src 'self' app://theme.gaiamobile.org app://shared.gaiamobile.org"
+   // "default-src * data: blob:; script-src 'self' http://127.0.0.1:8081; object-src 'none'; style-src 'self' app://theme.gaiamobile.org app://shared.gaiamobile.org"
    // That means we can optimize for this case by:
    // - loading same origin scripts and stylesheets, and stylesheets from the
    //   theme url space.
    // - loading scripts and stylesheets from the shared url space.
+   // - loading scripts from local http server
    // - never loading objects.
    // - accepting everything else.
 
@@ -168,8 +169,10 @@ CSPService::ShouldLoad(uint32_t aContentType,
           aContentLocation->GetPrePath(contentOrigin);
 
           auto sharedOrigin = NS_LITERAL_CSTRING("app://shared.gaiamobile.org");
+          auto localOrigin = NS_LITERAL_CSTRING("http://127.0.0.1:8081");
 
           if (!(sourceOrigin.Equals(contentOrigin) ||
+                localOrigin.Equals(contentOrigin) ||
                 sharedOrigin.Equals(contentOrigin) ||
                 (themeOrigin && themeOrigin.Equals(contentOrigin)))) {
             *aDecision = nsIContentPolicy::REJECT_SERVER;
