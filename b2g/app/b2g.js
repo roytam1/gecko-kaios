5550c4692190e8a874e7a9b72f3db4694731dfc5
Andy Wu <andy.wu@kaiostech.com>
2018-02-21 17:53:11 +0800

    Bug 36038 - [Push] Update push token url for production server as default. r=Hanson
    (cherry picked from commit 69b90f1aacc19e7967481e7fe8c671a2cb777332)

--- b2g/app/b2g.js
+++ b2g/app/b2g.js
@@ -1191,6 +1191,8 @@ pref("dom.push.adaptive.enabled", true);
 pref("dom.push.pingInterval", 180000); // 3 minutes
 // KaiOS push service needs authorization
 pref("dom.push.authorization.enabled", true);
+// The end point to get a access token for KaiOS push service
+pref("dom.push.token.uri", "https://api.kaiostech.com/v3.0/applications/cuo3fxgK6DeXodwsXvLY/tokens");
 
 // Retain at most 10 processes' layers buffers
 pref("layers.compositor-lru-size", 10);
