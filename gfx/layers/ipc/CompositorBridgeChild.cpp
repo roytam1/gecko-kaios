5dfbbf0a5965f7fe6a4031eb1d8dc90f16e1c7d3
chrono.wu <chrono.wu@kaiostech.com>
2018-10-03 15:08:05 +0800

    Bug 49378 - Remove assert check within CompositorBridgeChild::ActorDestroy since we don't need it. r=Solomon
    (cherry picked from commit c68e724d192f94e646f9cbf8f948c2a30832c45c)

--- gfx/layers/ipc/CompositorBridgeChild.cpp
+++ gfx/layers/ipc/CompositorBridgeChild.cpp
@@ -442,14 +442,6 @@ void
 CompositorBridgeChild::ActorDestroy(ActorDestroyReason aWhy)
 {
   if (aWhy == AbnormalShutdown) {
-#ifdef MOZ_B2G
-  // Due to poor lifetime management of gralloc (and possibly shmems) we will
-  // crash at some point in the future when we get destroyed due to abnormal
-  // shutdown. Its better just to crash here. On desktop though, we have a chance
-  // of recovering.
-    NS_RUNTIMEABORT("ActorDestroy by IPC channel failure at CompositorBridgeChild");
-#endif
-
     // If the parent side runs into a problem then the actor will be destroyed.
     // There is nothing we can do in the child side, here sets mCanSend as false.
     mCanSend = false;
