rebhu-open-source-foundation/gecko-b2g

Bug 50290 - Add special ERROR_MODE_NOT_ALLOW_WHEN_NCK for mtn/orange dual sim lock feature.r=Jack.hung

dom/system/gonk/ril_consts.js
@@ -228,6 +228,9 @@ this.ERROR_ILLEGAL_SIM_OR_ME = 15;
 this.ERROR_MISSING_RESOURCE = 16;
 this.ERROR_NO_SUCH_ELEMENT = 17;
 
+this.KAIOS_ERROR_BASE = 3100;
+this.ERROR_NOT_ALLOWED_WHEN_NCK = this.KAI_ERROR_BASE + 1;
+
 this.GECKO_ERROR_RADIO_NOT_AVAILABLE = "RadioNotAvailable";
 this.GECKO_ERROR_GENERIC_FAILURE = "GenericFailure";
 this.GECKO_ERROR_PASSWORD_INCORRECT = "IncorrectPassword";
@@ -247,6 +250,7 @@ this.GECKO_ERROR_MISSING_RESOURCE = "MissingResource";
 this.GECKO_ERROR_NO_SUCH_ELEMENT = "NoSuchElement";
 this.GECKO_ERROR_INVALID_PARAMETER = "InvalidParameter";
 this.GECKO_ERROR_UNSPECIFIED_ERROR = "UnspecifiedError";
+this.GECKO_ERROR_NOT_ALLOWED_WHEN_NCK = "NotAllowedWhenNCK";
 
 this.RIL_ERROR_TO_GECKO_ERROR = {};
 RIL_ERROR_TO_GECKO_ERROR[ERROR_RADIO_NOT_AVAILABLE] = GECKO_ERROR_RADIO_NOT_AVAILABLE;
@@ -266,6 +270,7 @@ RIL_ERROR_TO_GECKO_ERROR[ERROR_FDN_CHECK_FAILURE] = GECKO_ERROR_FDN_CHECK_FAILUR
 RIL_ERROR_TO_GECKO_ERROR[ERROR_ILLEGAL_SIM_OR_ME] = GECKO_ERROR_ILLEGAL_SIM_OR_ME;
 RIL_ERROR_TO_GECKO_ERROR[ERROR_MISSING_RESOURCE] = GECKO_ERROR_MISSING_RESOURCE;
 RIL_ERROR_TO_GECKO_ERROR[ERROR_NO_SUCH_ELEMENT] = GECKO_ERROR_NO_SUCH_ELEMENT;
+RIL_ERROR_TO_GECKO_ERROR[ERROR_NOT_ALLOWED_WHEN_NCK] = GECKO_ERROR_NOT_ALLOWED_WHEN_NCK;
 
 // 3GPP 23.040 clause 9.2.3.6 TP-Message-Reference(TP-MR):
 // The number of times the MS automatically repeats the SMS-SUBMIT shall be in

roytam1/gecko-kaios

(cherry picked from commit e1b43453294b4feddb835ce91cb5360781a9cc1f)

dom/system/gonk/ril_consts.js
@@ -228,6 +228,9 @@ this.ERROR_ILLEGAL_SIM_OR_ME = 15;
 this.ERROR_MISSING_RESOURCE = 16;
 this.ERROR_NO_SUCH_ELEMENT = 17;
 
+this.KAIOS_ERROR_BASE = 3100;
+this.ERROR_NOT_ALLOWED_WHEN_NCK = this.KAIOS_ERROR_BASE + 1;
+
 this.GECKO_ERROR_RADIO_NOT_AVAILABLE = "RadioNotAvailable";
 this.GECKO_ERROR_GENERIC_FAILURE = "GenericFailure";
 this.GECKO_ERROR_PASSWORD_INCORRECT = "IncorrectPassword";
@@ -247,6 +250,7 @@ this.GECKO_ERROR_MISSING_RESOURCE = "MissingResource";
 this.GECKO_ERROR_NO_SUCH_ELEMENT = "NoSuchElement";
 this.GECKO_ERROR_INVALID_PARAMETER = "InvalidParameter";
 this.GECKO_ERROR_UNSPECIFIED_ERROR = "UnspecifiedError";
+this.GECKO_ERROR_NOT_ALLOWED_WHEN_NCK = "NotAllowedWhenNCK";
 
 this.RIL_ERROR_TO_GECKO_ERROR = {};
 RIL_ERROR_TO_GECKO_ERROR[ERROR_RADIO_NOT_AVAILABLE] = GECKO_ERROR_RADIO_NOT_AVAILABLE;
@@ -266,6 +270,7 @@ RIL_ERROR_TO_GECKO_ERROR[ERROR_FDN_CHECK_FAILURE] = GECKO_ERROR_FDN_CHECK_FAILUR
 RIL_ERROR_TO_GECKO_ERROR[ERROR_ILLEGAL_SIM_OR_ME] = GECKO_ERROR_ILLEGAL_SIM_OR_ME;
 RIL_ERROR_TO_GECKO_ERROR[ERROR_MISSING_RESOURCE] = GECKO_ERROR_MISSING_RESOURCE;
 RIL_ERROR_TO_GECKO_ERROR[ERROR_NO_SUCH_ELEMENT] = GECKO_ERROR_NO_SUCH_ELEMENT;
+RIL_ERROR_TO_GECKO_ERROR[ERROR_NOT_ALLOWED_WHEN_NCK] = GECKO_ERROR_NOT_ALLOWED_WHEN_NCK;
 
 // 3GPP 23.040 clause 9.2.3.6 TP-Message-Reference(TP-MR):
 // The number of times the MS automatically repeats the SMS-SUBMIT shall be in