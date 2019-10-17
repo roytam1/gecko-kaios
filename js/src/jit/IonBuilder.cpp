Kevinisagirl21/tor-browser

Bug 1331058 - Fix analysis check in IonBuilder::initEnvironmentChain. r=shu a=lizzard

js/src/jit/IonBuilder.cpp
@@ -1233,9 +1233,11 @@ IonBuilder::initEnvironmentChain(MDefinition* callee)
         current->add(env);
 
         // This reproduce what is done in CallObject::createForFunction. Skip
-        // this for analyses, as the script might not have a baseline script
-        // with template objects yet.
-        if (fun->needsSomeEnvironmentObject() && !info().isAnalysis()) {
+        // this for the arguments analysis, as the script might not have a
+        // baseline script with template objects yet.
+        if (fun->needsSomeEnvironmentObject() &&
+            info().analysisMode() != Analysis_ArgumentsUsage)
+        {
             if (fun->needsNamedLambdaEnvironment()) {
                 env = createNamedLambdaObject(callee, env);
                 if (!env)
@@ -6251,6 +6253,9 @@ IonBuilder::createCallObject(MDefinition* callee, MDefinition* env)
     for (PositionalFormalParameterIter fi(script()); fi; fi++) {
         if (!fi.closedOver())
             continue;
+
+        if (!alloc().ensureBallast())
+            return nullptr;
 
         unsigned slot = fi.location().slot();
         unsigned formal = fi.argumentSlot();

roytam1/gecko-kaios

js/src/jit/IonBuilder.cpp
@@ -1236,9 +1236,11 @@ IonBuilder::initScopeChain(MDefinition* callee)
         current->add(scope);
 
         // This reproduce what is done in CallObject::createForFunction. Skip
-        // this for analyses, as the script might not have a baseline script
-        // with template objects yet.
-        if (fun->needsCallObject() && !info().isAnalysis()) {
+        // this for the arguments analysis, as the script might not have a
+        // baseline script with template objects yet.
+        if (fun->needsCallObject() &&
+            info().analysisMode() != Analysis_ArgumentsUsage)
+        {
             if (fun->isNamedLambda()) {
                 scope = createDeclEnvObject(callee, scope);
                 if (!scope)